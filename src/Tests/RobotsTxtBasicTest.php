c6bb5c7928d7b2e41cf65cd9c39b5346c43215c5
hass <hass@85918.no-reply.drupal.org>
2016-09-11 13:04:31 +0200

    Add test module / Fix failing tests

--- src/Tests/RobotsTxtBasicTest.php
+++ src/Tests/RobotsTxtBasicTest.php
@@ -16,7 +16,7 @@ class RobotsTxtBasicTest extends WebTestBase {
    *
    * @var array
    */
-  public static $modules = ['robotstxt', 'node'];
+  public static $modules = ['robotstxt', 'node', 'robotstxt_test'];
 
   /**
    * Checks that an administrator can view the configuration page.
@@ -40,6 +40,7 @@ class RobotsTxtBasicTest extends WebTestBase {
     $this->drupalLogin($this->normal_user);
     $this->drupalGet('admin/config/search/robotstxt');
 
+    $this->assertResponse(403);
     $this->assertNoFieldById('edit-robotstxt-content', NULL, 'The textarea for configuring robots.txt is not shown for users without appropriate permissions.');
   }
 
@@ -47,11 +48,7 @@ class RobotsTxtBasicTest extends WebTestBase {
    * Test that the robots.txt path delivers content with an appropriate header.
    */
   public function testRobotsTxtPath() {
-    if ($this->robotsTxtFileExists()) {
-      return;
-    }
-
-    $this->drupalGet('robots.txt');
+    $this->drupalGet('robots-test.txt');
     $this->assertResponse(200, 'No local robots.txt file was detected, and an anonymous user is delivered content at the /robots.txt path.');
     $header = $this->drupalGetHeader('Content-Type');
     // Note: the header may have charset appended.
@@ -62,40 +59,22 @@ class RobotsTxtBasicTest extends WebTestBase {
    * Checks that a configured robots.txt file is delivered as configured.
    */
   public function testRobotsTxtConfigureRobotsTxt() {
-    if ($this->robotsTxtFileExists()) {
-      return;
-    }
-
-    // Create user.
+    // Create an admin user, log in and access settings form.
     $this->admin_user = $this->drupalCreateUser(['administer robots.txt']);
     $this->drupalLogin($this->admin_user);
     $this->drupalGet('admin/config/search/robotstxt');
 
-    $test_string = $this->randomMachineName();
+    $test_string = "# SimpleTest {$this->randomMachineName()}";
     $this->drupalPostForm('admin/config/search/robotstxt', ['robotstxt_content' => $test_string], t('Save'));
 
     $this->drupalLogout();
-    $this->drupalGet('robots.txt');
+    $this->drupalGet('robots-test.txt');
     $this->assertResponse(200, 'No local robots.txt file was detected, and an anonymous user is delivered content at the /robots.txt path.');
     $header = $this->drupalGetHeader('Content-Type');
     // Note: the header may have charset appended.
     $this->assertIdentical(strpos($header, 'text/plain'), 0, 'The robots.txt file was served with header Content-Type: text/plain');
     $content = $this->drupalGetContent();
     $this->assertTrue($content == $test_string, sprintf('Test string [%s] is displayed in the configured robots.txt file [%s].', $test_string, $content));
-  }
-
-  /**
-   * Checks whether a local robots.txt file exists.
-   *
-   * @return bool
-   *   Returns TRUE if a local robots.txt file is found.
-   */
-  private function robotsTxtFileExists() {
-    $exists = file_exists(DRUPAL_ROOT . '/robots.txt');
-    if ($exists) {
-      $this->error(sprintf('Unable to proceed with configured robots.txt tests: A local file already exists at %s, so the menu override in this module will never run.', DRUPAL_ROOT . '/robots.txt'));
-    }
-    return $exists;
   }
 
 }
